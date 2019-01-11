39c7412d5419eea238962c60316f136506086a76
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2019-01-11 17:16:30 -0600

    WiP.

--- main.js
+++ main.js
@@ -1,72 +1,110 @@
 'use strict';
 
-var debug     = require('debug')('heya-io:bundle');
-var request   = require('request');
-var par       = require('heya-async').par;
+const debug = require('debug')('heya-io:bundle');
+const io = require('heya-io-node');
+const {par} = require('heya-async');
 
+const identity = x => x;
 
-function requestAsync () {
-	var args = Array.prototype.slice.call(arguments, 0);
-	return new Promise(function(resolve, reject){
-		args.push(function (error) {
-			if (error) {
-				reject(error);
-			} else {
-				resolve(Array.prototype.slice.call(arguments, 1));
-			}
-		});
-		try {
-			request.apply(null, args);
-		} catch (e) {
-			reject(e);
+const defaultSetHeaders = (results, res) =>
+	results.forEach(response => {
+		if (!(response instanceof Error)) {
+			response
+				.getAllResponseHeaders()
+				.split('\r\n')
+				.forEach(header => {
+					const parts = header.split(': ');
+					res.set(parts[0], parts[1] || '');
+				});
 		}
 	});
-}
-requestAsync.original = request;
 
+const localLog = (_, msg) => debug(msg);
 
-function identity (x) { return x; }
-
-function defaultSetHeaders (results, res) {
-	results.forEach(function (response) {
-		var rawHeaders = response[0].rawHeaders;
-		for(var i = 0; i < rawHeaders.length; i += 2) {
-			res.set(rawHeaders[i], rawHeaders[i + 1]);
+const dictToPairs = (dict, processPair) => {
+	for (let key in dict) {
+		const value = dict[key];
+		if (value instanceof Array) {
+			value.forEach(val => processPair(key, val));
+		} else {
+			processPair(key, value);
 		}
-	});
-}
+	}
+};
 
-function localLog (_, msg) { debug(msg); }
+const makeQuery = dict => {
+	const query = [];
+	if (dict && typeof dict == 'object') {
+		dictToPairs(dict, (key, value) => query.push(encodeURIComponent(key) + '=' + encodeURIComponent(value)));
+	}
+	return query.join('&');
+};
 
+const makeHeaders = (xhr, mime) => {
+	let headers = xhr.getAllResponseHeaders();
+	if (!mime) return headers;
+	headers = headers
+		.split('\r\n')
+		.filter(header => header.split(': ')[0].toLowerCase() != 'content-type')
+		.join('\r\n');
+	return headers + '\r\nContent-Type: ' + mime;
+};
 
-function instrumentBundle (opt) {
-	var isUrlAcceptable = opt.isUrlAcceptable,
+const normalizeOptions = options => (typeof options == 'string' ? {url: options, method: 'GET'} : options);
+
+const parseBody = body => {
+	try {
+		return JSON.parse(body);
+	} catch (e) {
+		return null;
+	}
+};
+
+const instrumentBundle = opt => {
+	const isUrlAcceptable = opt.isUrlAcceptable,
 		maxRequests = opt.maxRequests || 20,
-		resolveUrl  = opt.resolveUrl  || identity,
-		setHeaders  = opt.setHeaders  || defaultSetHeaders,
-		processResult  = opt.processResult  || identity,
+		resolveUrl = opt.resolveUrl || identity,
+		setHeaders = opt.setHeaders || defaultSetHeaders,
+		processResult = opt.processResult || identity,
 		processFailure = opt.processFailure || identity,
-		processBundle  = opt.processBundle  || identity,
-		onBundleStart  = opt.onBundleStart  || identity,
+		processBundle = opt.processBundle || identity,
+		onBundleStart = opt.onBundleStart || identity,
 		onBundleFinish = opt.onBundleFinish || identity,
-		onItemStart  = opt.onItemStart  || identity,
+		onItemStart = opt.onItemStart || identity,
 		onItemFinish = opt.onItemFinish || identity,
 		log = opt.log || localLog;
-	return function bundle (req, res) {
-		log('info', '=> ' + req.method + ' ' + req.url +
-			(req.body && req.body.length ? ' (payload: ' + req.body.length + ' bytes of ' + req.get('content-type') + ')' : ''),
-			{method: req.method, url: req.url, length: req.body && req.body.length || 0, contentType: req.get('content-type')});
+	return (req, res) => {
+		log(
+			'info',
+			'=> ' +
+				req.method +
+				' ' +
+				req.url +
+				(req.body && req.body.length
+					? ' (payload: ' + req.body.length + ' bytes of ' + req.get('content-type') + ')'
+					: ''),
+			{
+				method: req.method,
+				url: req.url,
+				length: (req.body && req.body.length) || 0,
+				contentType: req.get('content-type')
+			}
+		);
 		// no request body
 		if (!req.body || !req.body.length) {
 			log('error', 'no payload');
-			res.status(400).type('text/plain').send('No payload');
+			res.status(400)
+				.type('text/plain')
+				.send('No payload');
 			return;
 		}
 		// wrong payload
-		var payload = parseBody(req.body.toString());
+		const payload = parseBody(req.body.toString());
 		if (!(payload instanceof Array)) {
 			log('error', 'wrong payload');
-			res.status(400).type('text/plain').send('Wrong payload');
+			res.status(400)
+				.type('text/plain')
+				.send('Wrong payload');
 			return;
 		}
 		// empty payload
@@ -78,17 +116,33 @@ function instrumentBundle (opt) {
 		// payload is too large
 		if (payload.length > maxRequests) {
 			log('error', 'large payload');
-			res.status(400).type('text/plain').send('Large payload');
+			res.status(400)
+				.type('text/plain')
+				.send('Large payload');
 			return;
 		}
-		log('info', '=> RECEIVED bundle of ' + payload.length + ': ' + payload.map(function (o) { return o.url || o; }).join(', '),
-			payload.map(function (o) { return o.url || o; }));
-		var bundleStart = Date.now(), itemTime = [];
+		log(
+			'info',
+			'=> RECEIVED bundle of ' +
+				payload.length +
+				': ' +
+				payload
+					.map(function(o) {
+						return o.url || o;
+					})
+					.join(', '),
+			payload.map(function(o) {
+				return o.url || o;
+			})
+		);
+		const bundleStart = Date.now(),
+			itemTime = [];
 		onBundleStart(req);
-		var requests = payload.map(function (options, index) {
+		const requests = payload.map((options, index) => {
 				itemTime[index] = Date.now();
 				onItemStart(req, options, index, payload);
-				var newOptions = {}, url, query, data;
+				const newOptions = {returnXHR: true};
+				let url, query, data;
 				if (typeof options == 'string') {
 					url = options;
 				} else {
@@ -119,7 +173,7 @@ function instrumentBundle (opt) {
 				}
 				// process data
 				if (newOptions.method !== 'GET') {
-					var contentType = newOptions.headers['Content-Type'];
+					const contentType = newOptions.headers['Content-Type'];
 					if (!contentType) {
 						if (data) {
 							newOptions.headers['Content-Type'] = 'application/json';
@@ -133,121 +187,103 @@ function instrumentBundle (opt) {
 					newOptions.headers.Accept = 'application/json';
 				}
 				newOptions.headers.Cookie = req.get('Cookie');
-				log('info', '<= ' + newOptions.method + ' ' + url + (url !== newOptions.url ? ' => ' + newOptions.url : '') +
-					(newOptions.body && newOptions.body.length ?
-						' (payload: ' + newOptions.body.length + ' bytes of ' + newOptions.headers['Content-Type'] + ')' : ''),
-					{method: newOptions.method, url: newOptions.url, length: newOptions.body && newOptions.body.length || 0,
-					contentType: newOptions.headers['Content-Type']});
+				log(
+					'info',
+					'<= ' +
+						newOptions.method +
+						' ' +
+						url +
+						(url !== newOptions.url ? ' => ' + newOptions.url : '') +
+						(newOptions.body && newOptions.body.length
+							? ' (payload: ' +
+							  newOptions.body.length +
+							  ' bytes of ' +
+							  newOptions.headers['Content-Type'] +
+							  ')'
+							: ''),
+					{
+						method: newOptions.method,
+						url: newOptions.url,
+						length: (newOptions.body && newOptions.body.length) || 0,
+						contentType: newOptions.headers['Content-Type']
+					}
+				);
 				return newOptions;
 			}),
-			promises = requests.map(function (options, index) {
-				if(options instanceof Error) {
+			promises = requests.map((options, index) => {
+				if (options instanceof Error) {
 					onItemFinish(req, options, index, payload);
 					itemTime[index] = Date.now() - itemTime[index];
 					return options;
 				}
-				return requestAsync(options).then(function (value) {
-					onItemFinish(req, value, index, payload);
-					itemTime[index] = Date.now() - itemTime[index];
-					return value;
-				}).catch(function (value) {
-					onItemFinish(req, value, index, payload);
-					itemTime[index] = Date.now() - itemTime[index];
-					return value;
-				});
+				return io(options)
+					.then(value => {
+						onItemFinish(req, value, index, payload);
+						itemTime[index] = Date.now() - itemTime[index];
+						return value;
+					})
+					.catch(value => {
+						onItemFinish(req, value, index, payload);
+						itemTime[index] = Date.now() - itemTime[index];
+						return value;
+					});
 			});
-		par(promises).then(function (results) {
+		par(promises).then(results => {
 			setHeaders(results, res);
-			results = results.map(function (response, index) {
-				var options = normalizeOptions(payload[index]);
+			results = results.map((response, index) => {
+				const options = normalizeOptions(payload[index]);
 				if (response instanceof Error) {
-					return processFailure({
+					return processFailure(
+						{
+							options: options,
+							time: itemTime[index],
+							response: {
+								status: 500,
+								statusText: response.message,
+								responseType: '',
+								responseText:
+									'heya/bundler encountered an error: ' +
+									(response.name ? '[' + response.name + '] ' : '') +
+									(response.message || '(unspecified)'),
+								headers: 'Content-Type: text/plain; charset=utf-8'
+							}
+						},
+						req
+					);
+				}
+				return processResult(
+					{
 						options: options,
 						time: itemTime[index],
 						response: {
-							status: 500,
-							statusText: response.message,
-							responseType: '',
-							responseText: 'heya/bundler encountered an error: ' +
-								(response.name ? '[' + response.name + '] ' : '') +
-								(response.message || '(unspecified)'),
-							headers: 'Content-Type: text/plain; charset=utf-8'
+							status: response.statusCode,
+							statusText: response.statusMessage,
+							responseType: options.responseType || '',
+							responseText: response.responseText,
+							headers: makeHeaders(response, options.mime)
 						}
-					}, req);
-				}
-				var head = response[0];
-				return processResult({
-					options: options,
-					time: itemTime[index],
-					response: {
-						status: head.statusCode,
-						statusText: head.statusMessage,
-						responseType: options.responseType || '',
-						responseText: response[1].toString(),
-						headers: makeHeaders(head.rawHeaders, options.mime)
-					}
-				}, req);
+					},
+					req
+				);
 			});
 			onBundleFinish(req);
-			var bundleTime = Date.now() - bundleStart;
-			log('info', '<= RETURNED bundle of ' + results.length + ' in ' + bundleTime + 'ms', {length: results.length, time: bundleTime});
-			res.set('Content-Type', 'application/json; charset=utf-8').
-				json(processBundle({
-					bundle: 'bundle',
-					results: results,
-					time: bundleTime
-				}, req));
+			const bundleTime = Date.now() - bundleStart;
+			log('info', '<= RETURNED bundle of ' + results.length + ' in ' + bundleTime + 'ms', {
+				length: results.length,
+				time: bundleTime
+			});
+			res.set('Content-Type', 'application/json; charset=utf-8').json(
+				processBundle(
+					{
+						bundle: 'bundle',
+						results: results,
+						time: bundleTime
+					},
+					req
+				)
+			);
 		});
 	};
-}
-
-
-function dictToPairs (dict, processPair) {
-	for(var key in dict) {
-		var value = dict[key];
-		if (value instanceof Array) {
-			for(var i = 0; i < value.length; ++i) {
-				processPair(key, value[i]);
-			}
-		} else {
-			processPair(key, value);
-		}
-	}
-}
-
-function makeQuery (dict) {
-	var query = [];
-	if (dict && typeof dict == 'object') {
-		dictToPairs(dict, function (key, value) {
-			query.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
-		});
-	}
-	return query.join('&');
-}
-
-function makeHeaders (rawHeaders, mime) {
-	if (mime) {
-		rawHeaders = rawHeaders.filter(function (value, index, array) {
-			return array[index >> 1 << 1].toLowerCase() != 'content-type';
-		});
-		rawHeaders.push('Content-Type', mime);
-	}
-	return rawHeaders.reduce(function (acc, value, index) {
-		return acc + (index % 2 ? ': ' : (index ? '\r\n' : '')) + value;
-	}, '');
-}
-
-function normalizeOptions (options) {
-	return typeof options == 'string' ? {url: options, method: 'GET'} : options;
-}
-
-function parseBody (body) {
-	try {
-		return JSON.parse(body);
-	} catch (e) {
-		return null;
-	}
-}
-
+};
 
 module.exports = instrumentBundle;
--- tests/server.js
+++ tests/server.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var http = require('http');
-var path = require('path');
-var debug = require('debug')('heya-io:server');
-var express = require('express');
-var bodyParser = require('body-parser');
+const http = require('http');
+const path = require('path');
+const debug = require('debug')('heya-io:server');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var bundler = require('heya-bundler');
+const bundler = require('../main');
 
 // The APP
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.raw({type: '*/*'}));
 
-var counter = 0;
+let counter = 0;
 
-app.all('/api*', function (req, res) {
+app.all('/api*', (req, res) => {
 	if (req.query.status) {
-		var status = parseInt(req.query.status, 10);
+		let status = parseInt(req.query.status, 10);
 		if (isNaN(status) || status < 100 || status >= 600) {
 			status = 200;
 		}
@@ -34,47 +34,44 @@ app.all('/api*', function (req, res) {
 			res.send('<div>Hello, world!</div>');
 			return;
 	}
-	var data = {
-			method: req.method,
-			protocol: req.protocol,
-			hostname: req.hostname,
-			url: req.url,
-			originalUrl: req.originalUrl,
-			headers: req.headers,
-			body: req.body && req.body.length && req.body.toString() || null,
-			query: req.query,
-			now: Date.now(),
-			counter: counter++
-		};
-	var timeout = 0;
+	const data = {
+		method: req.method,
+		protocol: req.protocol,
+		hostname: req.hostname,
+		url: req.url,
+		originalUrl: req.originalUrl,
+		headers: req.headers,
+		body: (req.body && req.body.length && req.body.toString()) || null,
+		query: req.query,
+		now: Date.now(),
+		counter: counter++
+	};
+	let timeout = 0;
 	if (req.query.timeout) {
-		var timeout = parseInt(req.query.timeout, 10);
+		timeout = parseInt(req.query.timeout, 10);
 		if (isNaN(timeout) || timeout < 0 || timeout > 60000) {
 			timeout = 0;
 		}
 	}
 	if (timeout) {
-		setTimeout(function () {
-			res.jsonp(data);
-		}, timeout);
+		setTimeout(() => res.jsonp(data), timeout);
 	} else {
 		res.jsonp(data);
 	}
 });
 
-app.put('/bundle', bundler({
-	isUrlAcceptable: isUrlAcceptable,
-	resolveUrl: resolveUrl
-}));
+const isUrlAcceptable = uri =>
+	typeof uri == 'string' && !/^\/\//.test(uri) && (uri.charAt(0) === '/' || /^http:\/\/localhost:3000\//.test(uri));
 
-function isUrlAcceptable (uri) {
-	return typeof uri == 'string' && !/^\/\//.test(uri) &&
-		(uri.charAt(0) === '/' || /^http:\/\/localhost:3000\//.test(uri));
-}
+const resolveUrl = uri => (uri.charAt(0) === '/' ? 'http://localhost:3000' + uri : uri);
 
-function resolveUrl (uri) {
-	return uri.charAt(0) === '/' ? 'http://localhost:3000' + uri : uri;
-}
+app.put(
+	'/bundle',
+	bundler({
+		isUrlAcceptable: isUrlAcceptable,
+		resolveUrl: resolveUrl
+	})
+);
 
 app.use(express.static(path.join(__dirname, '..')));
 
@@ -98,33 +95,11 @@ app.use(function(err, req, res, next) {
 // The SERVER
 
 /**
- * Get port from environment and store in Express.
- */
-
-var host = process.env.HOST || 'localhost',
-	port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on provided host, or all network interfaces.
- */
-
-server.listen(port, host);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
-	var port = parseInt(val, 10);
+const normalizePort = val => {
+	const port = parseInt(val, 10);
 
 	if (isNaN(port)) {
 		// named pipe
@@ -137,27 +112,38 @@ function normalizePort(val) {
 	}
 
 	return false;
-}
+};
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const host = process.env.HOST || 'localhost',
+	port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
 
 /**
  * Human-readable port description.
  */
 
-function portToString (port) {
-	return typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
-}
-
+const portToString = port => (typeof port === 'string' ? 'pipe ' + port : 'port ' + port);
 
 /**
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
+const onError = error => {
 	if (error.syscall !== 'listen') {
 		throw error;
 	}
 
-	var bind = portToString(port);
+	const bind = portToString(port);
 
 	// handle specific listen errors with friendly messages
 	switch (error.code) {
@@ -172,14 +158,22 @@ function onError(error) {
 		default:
 			throw error;
 	}
-}
+};
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 
-function onListening() {
-	//var addr = server.address();
-	var bind = portToString(port);
+const onListening = () => {
+	// const addr = server.address();
+	const bind = portToString(port);
 	debug('Listening on ' + (host || 'all network interfaces') + ' ' + bind);
-}
+};
+
+/**
+ * Listen on provided port, on provided host, or all network interfaces.
+ */
+
+server.listen(port, host);
+server.on('error', onError);
+server.on('listening', onListening);
